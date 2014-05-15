a868c9e08061e36cce07e15a668521fd4d4b45df
casercion <casercion14@smpanthers.org>
2014-05-15 11:49:56 -0700

    Finished

--- collatz.js
+++ collatz.js
@@ -1,1 +1,27 @@
 console.log("Connor's Collatz");
+
+function collatz(x){
+  length = 0;
+  console.log(x);
+  while (x>1){
+    if ( (x % 2) == 0){
+      x = x / 2;
+    }
+    else {
+      x = 3 * x + 1;
+    }
+  length++;
+  console.log(x);
+  }
+
+}
+
+if (process.argv.length >= 3){
+  start=process.argv[2]
+}
+else{
+  start=7;
+  console.log("7 is the default value. Add an argument to the command line next time!");
+}
+collatz(start);
+console.log( "The length of Collatz sequence for " + start + " is " + length);
